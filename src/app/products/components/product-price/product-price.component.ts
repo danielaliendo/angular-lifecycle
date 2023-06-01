3de93be250e2eb4c7bae7f93b3b3558eae750f5c
Daniela Liendo <daniela@ualabee.com>
2023-06-01 23:49:00 +0200

    product price component was created

---
+++ src/app/products/components/product-price/product-price.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-price',
+  templateUrl: './product-price.component.html',
+  styleUrls: ['./product-price.component.css']
+})
+
+export class ProductPriceComponent {
+
+}
