7ecbcd2a98dc4f050dc4e5167c9948586bcafab7
Daniela Liendo <daniela@ualabee.com>
2023-06-01 22:41:02 +0200

    product page component was created

---
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css']
+})
+export class ProductPageComponent {
+
+}
