75195fcbfe42dbd32c92a2bdbbeacf7ef9e03b9c
Daniela Liendo <daniela@ualabee.com>
2023-06-02 09:14:51 +0200

    lifecycle hooks were added

--- src/app/products/components/product-price/product-price.component.ts
+++ src/app/products/components/product-price/product-price.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'products-product-price',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-price.component.css']
 })
 
-export class ProductPriceComponent {
+export class ProductPriceComponent implements OnInit, OnChanges, OnDestroy{
+
+  @Input()
+  public price: number = 0
+  public interval$?: Subscription
+
+  constructor() {
+    console.log('------------------------------------------------------------------')
+    console.log('------ constructor was trigger from product price component ------')
+    console.log('------------------------------------------------------------------')
+  }
+
+  ngOnInit(): void {
+    console.log('--------------------------------------------------------------------')
+    console.log('-- ngOnInit was trigger from product price component ---------------')
+    console.log('--------------------------------------------------------------------')
+    this.interval$ = interval(1000).subscribe(value => console.log(value))
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngOnChanges was trigger from product price component -------------')
+    console.log('---------------------------------------------------------------------')
+    console.log({changes})
+  }
+
+  ngOnDestroy(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngOnDestroy was trigger from product price component -------------')
+    console.log('---------------------------------------------------------------------')
+
+    this.interval$?.unsubscribe()
+  }
 
 }
--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges, OnDestroy,
+  OnInit, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
-export class ProductPageComponent {
+
+export class ProductPageComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  public isProductVisible: boolean = false
+  public currentPrice : number = 10
+
+  constructor() {
+    console.log('------------------------------------------------------------------')
+    console.log('------------------- constructor was trigger ----------------------')
+    console.log('------------------------------------------------------------------')
+  }
+
+  ngOnInit(): void {
+    console.log('--------------------------------------------------------------------')
+    console.log('-- ngOnInit was trigger: -------------------------------------------')
+    console.log('-- You can use it to: ----------------------------------------------')
+    console.log('-- * Perform complex initializations outside of the constructor ----')
+    console.log('------ - Like fetch initial data. ----------------------------------')
+    console.log('-- * Set up the component after Angular sets the input properties --')
+    console.log('--------------------------------------------------------------------')
+  }
+
+  ngDoCheck(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngDoCheck was trigger: -------------------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngAfterContentInit was trigger: ----------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngAfterContentChecked was trigger: -------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngAfterViewInit was trigger: -------------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngAfterViewChecked was trigger: ----------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngOnChanges was trigger: -----------------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  ngOnDestroy(): void {
+    console.log('---------------------------------------------------------------------')
+    console.log('-- ngOnDestroy was trigger: -----------------------------------------')
+    console.log('---------------------------------------------------------------------')
+  }
+
+  increasePrice() {
+    this.currentPrice ++
+  }
 
 }
